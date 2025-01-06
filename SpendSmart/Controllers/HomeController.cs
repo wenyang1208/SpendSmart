9b3d5b74e7f6606b2a67d54e918e0375ba40c460
Chua Wen Yang <wenyang200320232023@outlook.com>
2025-01-06 12:26:30 +0800

    Create new expense

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace SpendSmart.Controllers
             return View();
         }
 
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
